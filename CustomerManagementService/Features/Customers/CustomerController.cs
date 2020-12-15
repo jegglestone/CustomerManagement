19fb2a5465c01cb9a3acc6d804faca8e0550c369
Joseph Egglestone <joseph.egglestone@outlook.com>
2020-12-15 20:00:44 +0000

    Customers endpoint folder layout

---
+++ CustomerManagementService/Features/Customers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CustomerManagementService.Model;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomerManagementService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IMediator _mediator;
+
+        public CustomerController(
+            ILogger<CustomerController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+
+    }
+}
